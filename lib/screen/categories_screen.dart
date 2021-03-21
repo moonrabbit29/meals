8ea4cd5d4e15590800458587668ae369d216cebd
dendi <dendiaryar@gmail.com>
2021-03-21 09:54:32 +0700

    adding drawer

--- lib/screen/categories_screen.dart
+++ lib/screen/categories_screen.dart
@@ -7,10 +7,9 @@ class CatergoriesScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-          appBar: AppBar(title: const Text("DelMeal"),),
-          body: GridView(
-        padding: const EdgeInsets.all(8),
+    return Container(
+          child: GridView(
+        padding: const EdgeInsets.all(15),
         children: DUMMY_CATEGORIES.map((catData)=>CategoryItem(catData.id,catData.title, catData.color)).toList(),
         gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
             maxCrossAxisExtent: 200, childAspectRatio: 3 / 2,crossAxisSpacing: 20, mainAxisSpacing: 20 ),
--- lib/screen/tab_scren.dart
+++ lib/screen/tab_scren.dart
@@ -1,6 +1,7 @@
 import "package:flutter/material.dart";
 import 'package:mealsapp/screen/categories_screen.dart';
 import 'package:mealsapp/screen/favorite_screen.dart';
+import 'package:mealsapp/widget/main_drawer.dart';
 
 class TabScreen extends StatefulWidget {
   @override
@@ -8,19 +9,45 @@ class TabScreen extends StatefulWidget {
 }
 
 class _TabScreenState extends State<TabScreen> {
+
+  List<Map<String,Object>> _pages = [
+    {'page' : CatergoriesScreen(),
+      'title' : 'Categories',
+    },
+    {
+      'page' : FavoriteScreen(),
+      'title' : 'Favorite'
+    }
+  ];
+  int _selectedPageIndex = 0;
+
+  void _selectPage(int index){
+    setState((){
+      _selectedPageIndex = index;
+    });
+  }
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 2, 
-      child: Scaffold(
-        appBar: AppBar(title: Text("Meals"),bottom: TabBar(tabs: <Widget>[
-          Tab(icon:Icon(Icons.category),text:"Categories"),
-          Tab(icon : Icon(Icons.favorite),text: "Favorites",),
-        ]),),
-        body: TabBarView(children: <Widget> [
-          CatergoriesScreen(),
-          FavoriteScreen(),
-        ],),
-      ));
+    return Scaffold(
+        appBar: AppBar(title: Text(_pages[_selectedPageIndex]['title'])),
+        drawer: MainDrawer(),
+        body: _pages[_selectedPageIndex]['page'],
+        bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Theme.of(context).primaryColor,
+        onTap: _selectPage,
+        currentIndex: _selectedPageIndex,
+        type: BottomNavigationBarType.shifting,
+        items : [
+          BottomNavigationBarItem(
+            backgroundColor: Theme.of(context).primaryColor,
+            icon: Icon(Icons.category),
+          title:Text("Categories")), 
+          BottomNavigationBarItem(
+            backgroundColor: Theme.of(context).primaryColor,
+            icon: Icon(Icons.star),
+          title:Text("Favorite")),
+        ]
+        ),
+      );
   }
 }
